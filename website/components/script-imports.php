1a1671d5f401576f3bb6a8968797cd0750a72c9e
= <=>
2023-11-06 10:13:22 +0300

    set up

---
+++ website/components/script-imports.php
@@ -0,0 +1,4 @@
+<script src="assets/js/jquery.js" type='text/javascript'></script>
+<script type="text/javascript" src="assets/js/jquery-cookie/jquery.cookie.js"></script>
+<script type="text/javascript" src="assets/js/swiper/swiper-bundle.min.js"></script>
+<script src="assets/js/app.js" type='text/javascript'></script>
---
+++ website/components/top-bar.php
@@ -0,0 +1,10 @@
+<div class="top-bar">
+    <div class="left">
+        <div class="logo">
+            <img src="assets/icons/logo.png" id="logo-icon" alt="JEM | Capital">
+        </div>
+    </div>
+    <div class="right">
+
+    </div>
+</div>
